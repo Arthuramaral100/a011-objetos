ac20aca4907ae47c001559a44a2c2e3458ce56a3
Arthuramaral100 <arthuramaral57@gmail.com>
2022-09-05 15:45:45 -0300

    Exercicios da aula 011-objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+//1-
+const sacolao = []
+
+//a)
+const banana = {
+    nome: "Banana",
+    preco: 3.99,
+    disponibilidade: true
+}
+const melancia = {
+    nome: "Melancia",
+    preco: 2.99,
+    disponibilidade: false
+}
+const laranja = {
+    nome: "Laranja",
+    preco: 10.00,
+    disponibilidade: true
+}
+
+//b)
+sacolao.push(banana, melancia, laranja)
+
+//c)
+console.log(sacolao);
